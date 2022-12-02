f09b33485d0afd65d6cf8bf7382c37e2d6392d27
Jan Rapacz <47718562+Haans001@users.noreply.github.com>
2022-12-02 20:50:17 +0100

    configures prisma (#3)

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,12 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prisma: PrismaService,
+  ) {}
 
   @Get()
-  getHello(): string {
+  async getHello(): Promise<string> {
+    // await this.prisma.user.create({
+    //   data: {
+    //     email: 'jan.rapcz@interia.pl',
+    //     name: 'Jan',
+    //     surname: 'Rapacz',
+    //     hash: '123',
+    //   },
+    // });
     return this.appService.getHello();
   }
 }
