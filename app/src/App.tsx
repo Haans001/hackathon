25883d7f961f854d9c6dec4edda6b74267f0d689
beerdevourer <piotrkaczoroww.ski@gmail.com>
2022-12-02 23:04:43 +0100

    added react router dom

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import './App.css'
 import { ChakraProvider, theme, Flex } from '@chakra-ui/react'
 import { Navbar } from './components/'
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
 
 function App() {
     return (
-        <ChakraProvider theme={theme}>
-            <Navbar></Navbar>
-        </ChakraProvider>
+        <BrowserRouter>
+            <ChakraProvider theme={theme}>
+                <Navbar></Navbar>
+                <Routes>{/* <Route element={} path='/'></Route> */}</Routes>
+            </ChakraProvider>
+        </BrowserRouter>
     )
 }
 
