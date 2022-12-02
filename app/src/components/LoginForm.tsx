da7205eae22fc86234fd3d1153ad7c9082cbac5a
wrzeznicki <wiktor.rzeznicki01@gmail.com>
2022-12-02 23:22:35 +0100

    create login form

---
+++ app/src/components/LoginForm.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import {
+    Container,
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Input,
+    Button,
+    theme,
+    Flex,
+  } from '@chakra-ui/react'
+
+
+const LoginForm = () => {
+
+    
+    const [login, setLogin] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault()
+        console.log(login, password);
+    }
+
+
+    return (
+        <Container maxW="container.sm">
+            <Flex height="100vh" justify="center" align="center">
+                 <FormControl>
+                    <FormLabel>
+                        Login:
+                        <Input 
+                        type="text" 
+                        name="login" 
+                        value={login} 
+                        onChange={(e) => setLogin(e.target.value)} />
+                    </FormLabel>
+                    <FormLabel>
+                        Password:
+                        <Input  
+                        type="password" 
+                        name="password" 
+                        value={password} 
+                        onChange={(e) => setPassword(e.target.value)}/>
+                    </FormLabel>
+                    <Button
+                        type="submit" 
+                        value="Submit" 
+                        onClick={handleSubmit}>
+                    Zaloguj sie
+                    </Button>
+                    </FormControl >
+                </Flex>     
+            </Container>
+
+    )
+}
+
+export default LoginForm
