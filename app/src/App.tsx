cd4d66bffe699d2dcbf558663e02e4bcc8d3a999
Jan Rapacz <jan.rapacz@gorrion.io>
2022-12-03 10:01:52 +0100

    adds organisation owner

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,107 +1,105 @@
-import { ChakraProvider, theme } from '@chakra-ui/react'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import CreateOrganisation from './components/CreateOrganisation'
-import Dashboard from './components/Dashboard'
-import LoginForm from './components/LoginForm'
-import Page404 from './components/Page404'
-import Profile from './components/Profile'
-import ProtectedRoute from './components/ProtectedRoute'
-import PublicRoute from './components/PublicRoute'
-import RegisterForm from './components/RegisterForm'
-import Tickets from './components/Tickets'
-import UserSiteOrganisation from './components/UserSiteOrganisation'
-import WelcomePage from './components/WelcomePage'
-import { AuthProvider } from './providers/AuthProvider'
-const queryClient = new QueryClient()
+import { ChakraProvider, theme } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import CreateOrganisation from "./components/CreateOrganisation";
+import Dashboard from "./components/Dashboard";
+import LoginForm from "./components/LoginForm";
+import Page404 from "./components/Page404";
+import Profile from "./components/Profile";
+import ProtectedRoute from "./components/ProtectedRoute";
+import PublicRoute from "./components/PublicRoute";
+import RegisterForm from "./components/RegisterForm";
+import Tickets from "./components/Tickets";
+import UserSiteOrganisation from "./components/UserSiteOrganisation";
+import WelcomePage from "./components/WelcomePage";
+import { AuthProvider } from "./providers/AuthProvider";
+const queryClient = new QueryClient();
 
 function App() {
-    let loggedIn: boolean = false
-
-    return (
-        <QueryClientProvider client={queryClient}>
-            <AuthProvider>
-                <BrowserRouter>
-                    <ChakraProvider theme={theme}>
-                        <Routes>
-                            <Route
-                                element={
-                                    <PublicRoute>
-                                        <LoginForm />
-                                    </PublicRoute>
-                                }
-                                path='/logowanie'
-                            />
-                            <Route
-                                element={
-                                    <PublicRoute>
-                                        <RegisterForm />
-                                    </PublicRoute>
-                                }
-                                path='/rejestracja'
-                            />
-                            <Route
-                                element={
-                                    <PublicRoute>
-                                        <WelcomePage />
-                                    </PublicRoute>
-                                }
-                                path='/'
-                            />
-                            <Route
-                                element={
-                                    <ProtectedRoute>
-                                        <div
-                                            style={{
-                                                paddingTop: '100px',
-                                            }}
-                                        >
-                                            <Dashboard></Dashboard>
-                                        </div>
-                                    </ProtectedRoute>
-                                }
-                                path='/panel'
-                            />
-                            <Route
-                                element={
-                                    <ProtectedRoute>
-                                        <Tickets />
-                                    </ProtectedRoute>
-                                }
-                                path='/zgloszenia'
-                            />
-                            <Route
-                                element={
-                                    <ProtectedRoute>
-                                        <CreateOrganisation />
-                                    </ProtectedRoute>
-                                }
-                                path='/stworz-organizacje'
-                            />
-                            <Route
-                                element={
-                                    <ProtectedRoute>
-                                        <UserSiteOrganisation />
-                                    </ProtectedRoute>
-                                }
-                                path='/organizacja/:id'
-                            />
-                            <Route
-                                element={
-                                    <ProtectedRoute>
-                                        <Profile />
-                                    </ProtectedRoute>
-                                }
-                                path='/profil'
-                            />
-                            <Route path='*' element={<Page404 />} />
-                        </Routes>
-                    </ChakraProvider>
-                </BrowserRouter>
-            </AuthProvider>
-        </QueryClientProvider>
-    )
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <BrowserRouter>
+          <ChakraProvider theme={theme}>
+            <Routes>
+              <Route
+                element={
+                  <PublicRoute>
+                    <LoginForm />
+                  </PublicRoute>
+                }
+                path="/logowanie"
+              />
+              <Route
+                element={
+                  <PublicRoute>
+                    <RegisterForm />
+                  </PublicRoute>
+                }
+                path="/rejestracja"
+              />
+              <Route
+                element={
+                  <PublicRoute>
+                    <WelcomePage />
+                  </PublicRoute>
+                }
+                path="/"
+              />
+              <Route
+                element={
+                  <ProtectedRoute>
+                    <div
+                      style={{
+                        paddingTop: "100px",
+                      }}
+                    >
+                      <Dashboard></Dashboard>
+                    </div>
+                  </ProtectedRoute>
+                }
+                path="/panel"
+              />
+              <Route
+                element={
+                  <ProtectedRoute>
+                    <Tickets />
+                  </ProtectedRoute>
+                }
+                path="/zgloszenia"
+              />
+              <Route
+                element={
+                  <ProtectedRoute>
+                    <CreateOrganisation />
+                  </ProtectedRoute>
+                }
+                path="/stworz-organizacje"
+              />
+              <Route
+                element={
+                  <ProtectedRoute>
+                    <UserSiteOrganisation />
+                  </ProtectedRoute>
+                }
+                path="/organizacja/:id"
+              />
+              <Route
+                element={
+                  <ProtectedRoute>
+                    <Profile />
+                  </ProtectedRoute>
+                }
+                path="/profil"
+              />
+              <Route path="*" element={<Page404 />} />
+            </Routes>
+          </ChakraProvider>
+        </BrowserRouter>
+      </AuthProvider>
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
--- app/src/components/CreateOrganisation.tsx
+++ app/src/components/CreateOrganisation.tsx
@@ -1,90 +1,104 @@
 import {
-    Button,
-    Flex,
-    FormControl,
-    FormLabel,
-    Heading,
-    Input,
-    Stack,
-    useColorModeValue,
-} from '@chakra-ui/react'
-import { Link } from 'react-router-dom'
-import { useState } from 'react'
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  useColorModeValue,
+} from "@chakra-ui/react";
+
+import { useState } from "react";
+import { useMutation } from "react-query";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "../config/axios";
 
 export default function CreateOrganisation(): JSX.Element {
-    const [nameInput, setNameInput] = useState('')
+  const [nameInput, setNameInput] = useState("");
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        console.log(nameInput)
+  const { mutateAsync: createTicket } = useMutation((data: any) =>
+    axios.post("/organisations/create", data)
+  );
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const data = {
+      name: nameInput,
+    };
+
+    try {
+      await createTicket(data);
+      navigate("/panel");
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    return (
-        <Flex
-            minH={'100vh'}
-            align={'center'}
-            justify={'center'}
-            bg={useColorModeValue('gray.50', 'gray.800')}
+  return (
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <form onSubmit={handleSubmit}>
+        <Stack
+          spacing={6}
+          w={"full"}
+          maxW={"md"}
+          bg={useColorModeValue("white", "gray.700")}
+          rounded={"xl"}
+          boxShadow={"lg"}
+          p={6}
+          my={12}
         >
-            <form onSubmit={handleSubmit}>
-                <Stack
-                    spacing={6}
-                    w={'full'}
-                    maxW={'md'}
-                    bg={useColorModeValue('white', 'gray.700')}
-                    rounded={'xl'}
-                    boxShadow={'lg'}
-                    p={6}
-                    my={12}
-                >
-                    <Heading
-                        lineHeight={1.1}
-                        fontSize={{ base: '2xl', sm: '3xl' }}
-                    >
-                        Stwórz nową organizację
-                    </Heading>
-
-                    <FormControl id='userName' isRequired>
-                        <FormLabel>Nazwa organizacji</FormLabel>
-                        <Input
-                            autoFocus
-                            placeholder='Podaj nazwę organizacji...'
-                            _placeholder={{ color: 'gray.500' }}
-                            type='text'
-                            required
-                            value={nameInput}
-                            onChange={(e) => {
-                                setNameInput(e.target.value)
-                            }}
-                        />
-                    </FormControl>
-                    <Stack spacing={6} direction={['column', 'row']}>
-                        <Link to='/panel' style={{ width: '100%' }}>
-                            <Button
-                                bg={'red.500'}
-                                color={'white'}
-                                w='full'
-                                _hover={{
-                                    bg: 'red.600',
-                                }}
-                            >
-                                Anuluj
-                            </Button>
-                        </Link>
-                        <Button
-                            bg={'purple.600'}
-                            color={'white'}
-                            w='full'
-                            _hover={{
-                                bg: 'purple.700',
-                            }}
-                            type='submit'
-                        >
-                            Stwórz
-                        </Button>
-                    </Stack>
-                </Stack>
-            </form>
-        </Flex>
-    )
+          <Heading lineHeight={1.1} fontSize={{ base: "2xl", sm: "3xl" }}>
+            Stwórz nową organizację
+          </Heading>
+          <FormControl id="userName" isRequired>
+            <FormLabel>Nazwa organizacji</FormLabel>
+            <Input
+              autoFocus
+              placeholder="Podaj nazwę organizacji..."
+              _placeholder={{ color: "gray.500" }}
+              type="text"
+              required
+              value={nameInput}
+              onChange={(e) => {
+                setNameInput(e.target.value);
+              }}
+            />
+          </FormControl>
+          <Stack spacing={6} direction={["column", "row"]}>
+            <Link to="/panel" style={{ width: "100%" }}>
+              <Button
+                bg={"red.500"}
+                color={"white"}
+                w="full"
+                _hover={{
+                  bg: "red.600",
+                }}
+              >
+                Anuluj
+              </Button>
+            </Link>
+            <Button
+              bg={"purple.600"}
+              color={"white"}
+              w="full"
+              _hover={{
+                bg: "purple.700",
+              }}
+              type="submit"
+            >
+              Stwórz
+            </Button>
+          </Stack>
+        </Stack>
+      </form>
+    </Flex>
+  );
 }
