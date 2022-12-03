a0212032f46ede4f378829db9eff649db4eae2bd
wrzeznicki <wiktor.rzeznicki01@gmail.com>
2022-12-03 12:05:20 +0100

    change btn position

--- app/src/components/Ticket.tsx
+++ app/src/components/Ticket.tsx
@@ -13,18 +13,19 @@ const Ticket = (props: any) => {
     <Flex justify={"center"}>
       <Card w={"50%"}>
         <CardBody>
-          <Flex
-            minHeight={"100%"}
-            justifyContent={"center"}
-            alignContent={"center"}
-          >
+          <Flex h={"100%"} justifyContent={"center"} alignContent={"center"}>
             <Flex flexDirection={"column"} gap={"10px"} h={"100%"}>
               <Text>{props.name}</Text>
               <Text>{props.title}</Text>
               <Text>{`od ${start.getDay()}-${start.getMonth()}-${start.getFullYear()}r. do ${end.getDay()}-${end.getMonth()}-${end.getFullYear()}r.`}</Text>
             </Flex>
-            <Flex alignContent={"center"}>
-              <Button w="32px" onClick={handlePlusButton}>
+            <Flex alignContent={"center"} h="100%" align={"center"}>
+              <Button
+                h="50%"
+                marginLeft={"40px"}
+                w="42px"
+                onClick={handlePlusButton}
+              >
                 <SmallAddIcon fontSize={"42px"} />
               </Button>
             </Flex>
