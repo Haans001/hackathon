c12f60b34636048274fb59ebe2cd0a63e221c936
wrzeznicki <wiktor.rzeznicki01@gmail.com>
2022-12-03 07:50:37 +0100

    added 404 page

---
+++ app/src/components/Page404.tsx
@@ -0,0 +1,37 @@
+import { Box, Container, Heading, Stack, Text } from "@chakra-ui/react";
+
+const Page404 = () => {
+  return (
+    <>
+      <Container maxW={"3xl"}>
+        <Stack
+          as={Box}
+          textAlign={"center"}
+          spacing={{ base: 8, md: 14 }}
+          py={{ base: 20, md: 36 }}
+        >
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
+            lineHeight={"110%"}
+          >
+            404 <br />
+            <Text as={"span"} color={"purple.600"}>
+              Page
+            </Text>
+          </Heading>
+          <Text color={"gray.500"}>Niestety podana strona nie istnieje.</Text>
+          <Stack
+            direction={"column"}
+            spacing={3}
+            align={"center"}
+            alignSelf={"center"}
+            position={"relative"}
+          ></Stack>
+        </Stack>
+      </Container>
+    </>
+  );
+};
+
+export default Page404;
