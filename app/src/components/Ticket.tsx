63cfd17719985421a497eb4c2b78877517ad4ce6
beerdevourer <piotrkaczoroww.ski@gmail.com>
2022-12-03 13:33:34 +0100

    repaired hours

--- app/src/components/Ticket.tsx
+++ app/src/components/Ticket.tsx
@@ -1,42 +1,63 @@
-import { SmallAddIcon } from "@chakra-ui/icons";
-import { Button, Card, CardBody, Flex, Text } from "@chakra-ui/react";
+import { SmallAddIcon } from '@chakra-ui/icons'
+import { Button, Card, CardBody, Flex, Text } from '@chakra-ui/react'
 
 const Ticket = (props: any) => {
-  const start = new Date(props.startTime);
-  const end = new Date(props.endTime);
+    const start = new Date(props.startTime)
+    const end = new Date(props.endTime)
 
-  const handlePlusButton = () => {
-    console.log("plus");
-  };
+    const handlePlusButton = () => {
+        console.log('plus')
+    }
 
-  return (
-    <Flex justify={"center"}>
-      <Card w={"50%"}>
-        <CardBody>
-          <Flex h={"100%"} justifyContent={"center"} alignContent={"center"}>
-            <Flex flexDirection={"column"} gap={"10px"} h={"100%"}>
-              <Text>{props.name}</Text>
-              <Text>{props.title}</Text>
-              <Text>
-                Dodane przez: {props.user.name} {props.user.surname}
-              </Text>
-              <Text>{`od ${start.getDay()}-${start.getMonth()}-${start.getFullYear()}r. ${start.getHours()}:${start.getMinutes()} do ${end.getDay()}-${end.getMonth()}-${end.getFullYear()}r. ${end.getHours()}:${end.getMinutes()}`}</Text>
-            </Flex>
-            <Flex alignContent={"center"} h="100%" align={"center"}>
-              <Button
-                h="50%"
-                marginLeft={"40px"}
-                w="42px"
-                onClick={handlePlusButton}
-              >
-                <SmallAddIcon fontSize={"42px"} />
-              </Button>
-            </Flex>
-          </Flex>
-        </CardBody>
-      </Card>
-    </Flex>
-  );
-};
+    return (
+        <Flex justify={'center'}>
+            <Card w={'50%'}>
+                <CardBody>
+                    <Flex
+                        h={'100%'}
+                        justifyContent={'center'}
+                        alignContent={'center'}
+                    >
+                        <Flex flexDirection={'column'} gap={'10px'} h={'100%'}>
+                            <Text>{props.name}</Text>
+                            <Text>{props.title}</Text>
+                            <Text>
+                                Dodane przez: {props.user.name}{' '}
+                                {props.user.surname}
+                            </Text>
+                            <Text>{`od ${start.getDay()}-${start.getMonth()}-${start.getFullYear()}r. ${
+                                start.getHours() < 10
+                                    ? '0' + start.getHours()
+                                    : start.getHours()
+                            }:${
+                                start.getMinutes() < 10
+                                    ? '0' + start.getMinutes()
+                                    : start.getMinutes()
+                            } do ${end.getDay()}-${end.getMonth()}-${end.getFullYear()}r. ${
+                                end.getHours() < 10
+                                    ? '0' + end.getHours()
+                                    : end.getHours()
+                            }:${
+                                end.getMinutes() < 10
+                                    ? '0' + end.getMinutes()
+                                    : end.getMinutes()
+                            }`}</Text>
+                        </Flex>
+                        <Flex alignContent={'center'} h='100%' align={'center'}>
+                            <Button
+                                h='50%'
+                                marginLeft={'40px'}
+                                w='42px'
+                                onClick={handlePlusButton}
+                            >
+                                <SmallAddIcon fontSize={'42px'} />
+                            </Button>
+                        </Flex>
+                    </Flex>
+                </CardBody>
+            </Card>
+        </Flex>
+    )
+}
 
-export default Ticket;
+export default Ticket
